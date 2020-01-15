bc33e5c35af5320111b014126ac3805a2100944a
Hugo Sousa <hugosousa@MacBook-Pro.local>
2020-01-15 16:29:37 +0100

    wrong dependency name

--- forms_builder/forms/migrations/0004_form_redirect_url.py
+++ forms_builder/forms/migrations/0004_form_redirect_url.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('forms', '0003_auto_20180522_0820.py'),
+        ('forms', '0003_auto_20180522_0820'),
     ]
 
     operations = [
