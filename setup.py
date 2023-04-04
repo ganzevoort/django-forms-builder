12310658d56d938cfc2535bcfe59b634cf0ce7f2
Job Ganzevoort <ganzevoort@gw20e.com>
2023-04-04 11:10:25 +0200

    setup.py: do not require (old version of) future

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ try:
             "unidecode",
             "django-email-extras >= 0.2",
             "django >= 1.8, < 5.0",
-            "future <= 0.15.0",
         ],
         classifiers = [
             "Development Status :: 5 - Production/Stable",
